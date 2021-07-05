0e07513a62d33c48fb5ca708e46b1f4804e5fb60
Salsabeel Hussien <58127260+sals1995@users.noreply.github.com>
2021-07-05 06:11:00 +0200

    section.js updated

---
+++ src/js/section.js
@@ -0,0 +1,9 @@
+let section= document.getElementsByClassName("hero01")[0]
+
+
+ const element = document.createElement("h1");
+
+element.innerHTML = "This Is A Great Website";
+section.appendChild(element)
+
+// this is a comment
