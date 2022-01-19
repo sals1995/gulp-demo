6c22b1436c750065303863b4fc86bba14efdf079
Salsabeel Hussien <58127260+sals1995@users.noreply.github.com>
2022-01-19 08:57:00 +0200

    updating versions

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const { src, dest, watch, parallel, series } = require("gulp")
 
 //minify images and copy it to dist folder
 const imagemin = require('gulp-imagemin');
+//don't forget to install gulp-imagemin with version 7.1.0
 function imgMinify() {
     return gulp.src('project/pics/*')
         .pipe(imagemin())
@@ -18,13 +19,13 @@ exports.img = imgMinify
 //creating dist folder and copy html files to it
 
 const htmlmin = require('gulp-htmlmin');
-function copyHtml() {
+function minifyHTML() {
     return src('project/*.html')
         .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
         .pipe(gulp.dest('dist'))
 }
 
-exports.html = copyHtml
+exports.html = minifyHTML
 
 
 //minify js files and copy it to dist folder
@@ -57,7 +58,7 @@ function cssMinify() {
 }
 exports.css = cssMinify
 //sass task
-var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 function sassMinify() {
     return src(["project/sass/**/*.scss", "project/css/**/*.css"],{sourcemaps:true})
         .pipe(sass()) // Using gulp-sass to convert sass to css
@@ -86,11 +87,11 @@ function reloadTask(done) {
 
 //watch task
 function watchTask() {
-    watch('project/*.html',series(copyHtml, reloadTask))
+    watch('project/*.html',series(minifyHTML, reloadTask))
     watch('project/js/**/*.js',series(jsMinify, reloadTask))
-    watch(["project/css/**/*.css","project/sass/**/*.scss"], parallel(sassMinify,reloadTask));
+    watch(["project/css/**/*.css","project/sass/**/*.scss"], series(sassMinify,reloadTask));
 }
-exports.default = series(parallel(imgMinify, jsMinify/* , cssMinify */, sassMinify, copyHtml), serve,watchTask)
+exports.default = series( parallel(imgMinify, jsMinify, sassMinify, minifyHTML), serve,watchTask)
 
 
 
